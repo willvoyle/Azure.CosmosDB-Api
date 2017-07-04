1bdeb4ae65f901d11c511c159ccb9318e05f8da3
William Voyle <will_voyle@hotmail.co.uk>
2017-07-04 17:14:41 +0100

    Created mock funciton for customer

--- Azure.CosmosDB.GeoApi.Harness/Program.cs
+++ Azure.CosmosDB.GeoApi.Harness/Program.cs
@@ -1,20 +1,22 @@
 ﻿using Azure.CosmosDB.GeoApi.Entities;
 using System;
-using System.Globalization;
 
 namespace Azure.CosmosDB.GeoApi.Harness
 {
     class Program
     {
+        private static CustomerService _customerService;
+
+        public Program()
+        {
+            _customerService = new CustomerService(new CosmosDbBaseRepository());
+        }
+
         static void Main(string[] args)
         {
-            var service = new CustomerService(new CosmosDbBaseRepository());
+            var id = _customerService.CreateCustomer(Customer.Mock());
 
-            var mockPerson = new Customer { FName = "FTest", LName = "LTest", Locale = CultureInfo.CurrentCulture.Name };
-
-            var id = service.CreateCustomer(mockPerson);
-
-            var person = service.GetCustomer(id);
+            var person = _customerService.GetCustomer(id);
 
             Console.WriteLine(person.FName);
             Console.ReadLine();
--- Azure.CosmosDB.GeoApi/Entities/Customer.cs
+++ Azure.CosmosDB.GeoApi/Entities/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Globalization;
 
 namespace Azure.CosmosDB.GeoApi.Entities
 {
@@ -13,5 +14,11 @@ namespace Azure.CosmosDB.GeoApi.Entities
         public string LName { get; set; }
         [JsonProperty("locale")]
         public string Locale { get; set; }
+
+        public static Customer Mock()
+        {
+            var rnd = new Random();
+            return new Customer { FName = $"FTest-{rnd.Next()}", LName = $"LTest-{rnd.Next()}", Locale = CultureInfo.CurrentCulture.Name };
+        }
     }
 }
